fa70fc8aa227c13488e4e0d67eaf3caefcc5b53b
Nuno Silva <nuno.mgomes.silva@gmail.com>
2016-11-16 08:29:28 +0100

    125k trainning iterations over 24 files, biggest trainning so far. Network converged nicely, never seen that, but learning rate is really slow after 25k iterations (2 epochs. 1 epoch = 24files*600splits=14400 trainning segments). Maybe network has to be bigger to memorize better each trainning segment over a large trainning set. Or trainning set is just too diverse and with big variations and it will take a long time for network to learn all the differences.

--- eegnet/train.py
+++ eegnet/train.py
@@ -13,6 +13,9 @@ from read_preproc_dataset import read_dataset
 tf.app.flags.DEFINE_string('dataset_dir', '/shared/dataset/train_small2/*.tfr', 
     'Where dataset TFReaders files are loaded from.')
 
+tf.app.flags.DEFINE_string('checkpoint_dir', '/shared/logs/train24/',
+    'Where checkpoints are loaded from.')
+
 tf.app.flags.DEFINE_string('log_dir', '/shared/logs/',
     'Where checkpoints and event logs are written to.')
 
@@ -52,10 +55,10 @@ FLAGS = tf.app.flags.FLAGS
 
 
 def get_init_fn():
-    checkpoint_path = tf.train.latest_checkpoint(FLAGS.log_dir)
+    checkpoint_path = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
     
     if checkpoint_path is None:
-        tf.logging.info('No checkpoint found in %s' % FLAGS.log_dir)
+        tf.logging.info('No checkpoint found in %s' % FLAGS.checkpoint_dir)
         return None
     
     tf.logging.info('Loading model from %s' % checkpoint_path)
@@ -123,7 +126,7 @@ def main(_):
                                          logdir=FLAGS.log_dir, 
                                          log_every_n_steps=10, 
                                          is_chief=True, 
-                                         number_of_steps=25001, 
+                                         number_of_steps=125001, 
                                          init_fn=get_init_fn(), 
                                          save_summaries_secs=5, 
                                          save_interval_secs=15*60, 
