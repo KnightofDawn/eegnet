219ca4ddfb2c55c7974a94bdf7775a6df9d774d7
Nuno Silva <nuno.mgomes.silva@gmail.com>
2016-11-25 13:41:27 +0100

    Using biggest machine available complex_model_l to allow a bigger batch size of 7 files at a time. Should have reduced the number of iterations, but its ok, model is being saved every 15 mins.

--- eegnet/train/gcloud_train.sh
+++ eegnet/train/gcloud_train.sh
@@ -18,6 +18,6 @@ gcloud beta ml jobs submit training ${JOB_NAME} \
   --dataset_dir="${TRAIN_BUCKET}/train/*.tfr" \
   --checkpoint_dir="${TRAIN_BUCKET}/checkpoints/second_pool2400_split1_batch3" \
   --log_dir="${TRAIN_PATH}" \
-  --batch_size=5 \
+  --batch_size=7 \
   --num_splits=1 \
   --num_iters=5000
