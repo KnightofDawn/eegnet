03915fcf78bf79bb1ddae9b406d2203d398cad13
edmundoferreira <fc.edmundo@gmail.com>
2016-12-06 10:49:10 +0100

    - simplified supervisord configurations

--- Docker/opt/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-
-echo "=>Starting Supervisor."
-# echo "You can safely CTRL-C and the container will continue to run..."
-/usr/bin/supervisord -c /etc/supervisor/conf.d/tensorboard_jupyter.conf >> /dev/null
